1b9eb61a64365faf08d0065e5aa3829a982bda80
jim.h <hjijin.gz@gmail.com>
2023-12-07 15:54:13 +0800

    modified the lesson style and add video component.

--- pages/course.vue
+++ pages/course.vue
@@ -1,7 +1,5 @@
 <template>
-	<div
-		class="flex flex-col items-center w-full h-full min-h-screen p-12 bg-gray-100"
-	>
+	<div class="flex flex-col items-center w-full h-full min-h-screen p-12 bg-gray-100">
 		<div class="mb-12 prose">
 			<h1>
 				<span class="font-medium">
@@ -12,15 +10,12 @@
 		</div>
 
 		<div class="flex flex-row justify-center flex-grow">
-			<div
-				class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
-			>
-				<h3>Chapters</h3>
+			<div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col">
+				<h3 class="font-bold">Chapters</h3>
 			</div>
 
 			<div class="prose p-12 bg-white rounded-md w-[65ch]">
-				<h2>Lesson</h2>
-        <p>This is a lesson</p>
+				<NuxtPage />
 			</div>
 		</div>
 	</div>
