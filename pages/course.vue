926d71d5b2ec6cb0907ae9906e47626925389763
jim.h <hjijin.gz@gmail.com>
2023-12-12 16:24:14 +0800

    add NuxtLinks.

--- pages/course.vue
+++ pages/course.vue
@@ -12,6 +12,26 @@
 		<div class="flex flex-row justify-center flex-grow">
 			<div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col">
 				<h3 class="font-bold">Chapters</h3>
+				<div
+					class="space-y-1 mt-4 flex flex-col"
+					v-for="chapter in chapters"
+					:key="chapter.slug"
+				>
+					<h4 class="font-bold">{{ chapter.title }}</h4>
+					<NuxtLink
+						v-for="(lesson, index) in chapter.lessons"
+						:key="lesson.slug"
+						class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
+						:to="lesson.path"
+						:class="{
+							'text-blue-500': lesson.path === $route.path,
+							'text-gray-600': lesson.path !== $route.path,
+						}"
+					>
+						<span class="text-gray-500">{{ index + 1 }}.</span>
+						<span>{{ lesson.title }}</span>
+					</NuxtLink>
+				</div>
 			</div>
 
 			<div class="prose p-12 bg-white rounded-md w-[65ch]">
@@ -22,5 +42,5 @@
 </template>
 
 <script setup>
-// const { chapters } = useCourse();
+const { chapters } = useCourse();
 </script>
