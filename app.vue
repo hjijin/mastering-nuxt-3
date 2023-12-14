28a43fa847534b74b0c98f47f6d02c74a7d472d2
jim.h <hjijin.gz@gmail.com>
2023-12-14 16:50:18 +0800

    defind layout.

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <!-- <NuxtWelcome /> -->
+    <!-- 可以在这里自定义 layout，但是是全局的 -->
+    <!-- <NuxtLayout name="custom"> -->
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
